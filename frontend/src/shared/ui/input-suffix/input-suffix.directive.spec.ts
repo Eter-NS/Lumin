c3d8fd8da0e2b1bd894a135ddffbfb52b7580485
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-11-26 01:20:42 +0100

    Add tests to input suffix directive

--- frontend/src/shared/ui/input-suffix/input-suffix.directive.spec.ts
+++ frontend/src/shared/ui/input-suffix/input-suffix.directive.spec.ts
@@ -1,8 +1,34 @@
+import { Component } from '@angular/core'
 import { InputSuffixDirective } from './input-suffix.directive'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+
+@Component({
+  standalone: true,
+  imports: [InputSuffixDirective],
+  template: ` <button type="submit" appInputSuffix>Hello World!</button> `,
+})
+class TestComponent {}
 
 describe('InputSuffixDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InputSuffixDirective()
-    expect(directive).toBeTruthy()
+  // Component && directive
+  let fixture: ComponentFixture<TestComponent>
+  let directiveElement: HTMLLabelElement
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TestComponent],
+    })
+
+    fixture = TestBed.createComponent(TestComponent)
+    fixture.detectChanges()
+
+    directiveElement = fixture.debugElement.query(By.directive(InputSuffixDirective))
+      .nativeElement as HTMLLabelElement
+  })
+
+  it(`should apply InputSuffixDirective.`, () => {
+    // Assert
+    expect(directiveElement).toBeTruthy()
   })
 })
