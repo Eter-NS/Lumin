1dcb3498dd418ea1bc3595bda883fc651646743f
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-12-10 02:09:01 +0100

    refactor(core): Update root.config.ts
    - Added Client hydration
    - Added Event Replay,
    - Added Internationalization support,
    - Added ViewTransitions API support
    - Applied CustomTitleStrategy

--- frontend/src/core/feature/root/root.config.ts
+++ frontend/src/core/feature/root/root.config.ts
@@ -1,7 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
-import { provideRouter } from '@angular/router'
+import { provideRouter, TitleStrategy, withViewTransitions } from '@angular/router'
 import { appRoutes } from './root.routes'
+import { provideClientHydration, withEventReplay, withI18nSupport } from '@angular/platform-browser'
+import { CustomTitleStrategy } from '@lumin/shared/custom-title-strategy/custom-title-strategy.service'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)],
+  providers: [
+    provideClientHydration(withEventReplay(), withI18nSupport()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes, withViewTransitions()),
+    {
+      provide: TitleStrategy,
+      useClass: CustomTitleStrategy,
+    },
+  ],
 }
