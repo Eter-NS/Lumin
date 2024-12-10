ff87da221b93f5dd17c70d1a3ce5fd5d7c7a0f7b
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-12-10 02:14:53 +0100

    test(core): Add unit tests for Root component

--- frontend/src/core/feature/root/root.component.spec.ts
+++ frontend/src/core/feature/root/root.component.spec.ts
@@ -1,11 +1,37 @@
-import { TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { RootComponent } from './root.component'
-import { RouterModule } from '@angular/router'
+import { provideRouter } from '@angular/router'
+import { provideLocationMocks } from '@angular/common/testing'
+import { Component, Provider } from '@angular/core'
+
+@Component({ standalone: true, template: `<p>Testing component works!</p>` })
+class TestComponent {}
 
 describe('RootComponent', () => {
+  let component: RootComponent
+  let fixture: ComponentFixture<RootComponent>
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RootComponent, RouterModule.forRoot([])],
+      imports: [RootComponent],
+      providers: [
+        provideRouter([
+          {
+            path: '',
+            component: TestComponent,
+          },
+        ]),
+        provideLocationMocks(),
+      ] as Provider[],
     }).compileComponents()
+
+    fixture = TestBed.createComponent(RootComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it(`should create`, () => {
+    // Assert
+    expect(component).toBeTruthy()
   })
 })
