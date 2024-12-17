2e23d1094bb41d84f5194c789c8dba73ede417d5
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-12-17 15:38:02 +0100

    refactor(shared): Allow the saveToLocalStorage operator to save all values

--- frontend/src/shared/utils/rxjs-operators/save-to-local-storage/save-to-local-storage.ts
+++ frontend/src/shared/utils/rxjs-operators/save-to-local-storage/save-to-local-storage.ts
@@ -1,14 +1,11 @@
 import { LocalStorageController } from '@lumin/shared/local-storage-controller/local-storage-controller.service'
 import { MonoTypeOperatorFunction, tap } from 'rxjs'
-import { AppTheme } from '../../models/appTheme.type'
 
-export function saveToLocalStorage<T extends AppTheme>(
+export function saveToLocalStorage<T>(
   service: LocalStorageController,
   token: string
 ): MonoTypeOperatorFunction<T> {
-  return tap((theme) => {
-    if (theme !== 'auto') {
-      service.set(token, theme)
-    }
+  return tap((value) => {
+    service.set(token, value)
   })
 }
