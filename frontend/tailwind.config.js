7c48e9ed31d088f342f50d7b74d767dae7c4a782
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-12-19 21:47:59 +0100

    feat: Add tailwindcss/container-queries plugin and update TailwindCSS config
    - added @tailwindcss/container-queries as a dependency in package.json
    - updated tailwind.config.js to include container queries plugin
    - added error background colors and fade-in animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -19,11 +19,25 @@ module.exports = {
         'ui-background-light': '#EDE1E1',
         'text-light': '#191726',
 
+        error: '#a54244',
+        // error: '#d32f2f',
+        'error-light': '#F29696',
+        // 'error-light': '#e57373',
+
         'custom-1': '#e79855',
         'custom-2': '#d6414f',
         'custom-3': '#1848ad',
       },
+      animation: {
+        fadeIn: 'fade-in-keyframes 200ms 100ms ease-in both',
+      },
+      keyframes: {
+        'fade-in-keyframes': {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
+      },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/container-queries')],
 }
