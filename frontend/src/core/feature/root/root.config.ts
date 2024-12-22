be7da2d7145b184a338dad29b972f715ffc94d98
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-12-22 02:47:56 +0100

    refactor(core): Update root.config.ts
    - added component input binding for query parameters

--- frontend/src/core/feature/root/root.config.ts
+++ frontend/src/core/feature/root/root.config.ts
@@ -1,5 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
-import { provideRouter, TitleStrategy, withViewTransitions } from '@angular/router'
+import {
+  provideRouter,
+  TitleStrategy,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router'
 import { appRoutes } from './root.routes'
 import { provideClientHydration, withEventReplay, withI18nSupport } from '@angular/platform-browser'
 import { CustomTitleStrategy } from '@lumin/shared/custom-title-strategy/custom-title-strategy.service'
@@ -8,7 +13,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay(), withI18nSupport()),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes, withViewTransitions()),
+    provideRouter(appRoutes, withViewTransitions(), withComponentInputBinding()),
     {
       provide: TitleStrategy,
       useClass: CustomTitleStrategy,
