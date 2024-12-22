2d67e7b5f6a33120db587b7c6cf3291085205da7
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-12-22 02:42:34 +0100

    refactor(shared): Expand Nav component's functionality
    - expanded Nav component by adding a slot for projected content (Tablet+ screens)
    - added fade-in and fade-out animations for navigation links
    - implemented transitions for when the component expands or collapses
    - improved the color layout
    - added an 'expanded' property, enabling two-way binding to manage the nav expansion state (Tablet+ screens
    - expanded the test suite to cover new functionalities

--- frontend/src/shared/ui/nav/nav.component.spec.ts
+++ frontend/src/shared/ui/nav/nav.component.spec.ts
@@ -1,20 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { NavComponent } from './nav.component'
-import { Component, DebugElement, input, Provider } from '@angular/core'
+import { Component, DebugElement, input, Provider, signal } from '@angular/core'
 import { RoutingLink } from '../../utils/models/routingLink.interface'
 import { By } from '@angular/platform-browser'
 import { provideRouter, Router } from '@angular/router'
 import { provideLocationMocks } from '@angular/common/testing'
 import { RouterTestingHarness } from '@angular/router/testing'
+import { IconComponent } from '@lumin/shared/models/iconComponent.interface'
 
 @Component({
   standalone: true,
-  template: `<p>The example icon component works!</p>`,
+  template: ` <p>The testing page works!</p> `,
 })
-class ExampleIconComponent {}
+class ExamplePageComponent {}
+
+@Component({
+  standalone: true,
+  template: `
+    <p>The example icon component works!</p>
+    <p>icon size: {{ size() }}</p>
+  `,
+})
+class ExampleIconComponent implements IconComponent {
+  size = input<string>('')
+}
 
 const navLinks: RoutingLink[] = Array.from({ length: 4 }, (_, i) => ({
-  component: ExampleIconComponent,
+  iconComponent: ExampleIconComponent,
   routerLink: `test-route-${i + 1}`,
   linkText: `link ${i + 1}`,
 }))
@@ -22,17 +34,21 @@ const navLinks: RoutingLink[] = Array.from({ length: 4 }, (_, i) => ({
 @Component({
   standalone: true,
   imports: [NavComponent],
-  template: ` <app-nav [routingLinks]="links()" /> `,
+  template: `
+    <app-nav [routingLinks]="links()" [(expanded)]="expanded">
+      @if (showExampleSectionContent()) {
+        <ng-template #navSectionContent>
+          <p data-test="projected-test-content">I'm a projected content inside nav element</p>
+        </ng-template>
+      }
+    </app-nav>
+  `,
 })
 class TestComponent {
-  links = input<RoutingLink[]>(navLinks)
+  links = signal<RoutingLink[]>(navLinks)
+  showExampleSectionContent = signal<boolean>(false)
+  expanded = signal(true)
 }
-
-@Component({
-  standalone: true,
-  template: ` <p>The testing page works!</p> `,
-})
-class ExamplePageComponent {}
 
 describe('NavComponent', () => {
   // component
@@ -94,5 +110,57 @@ describe('NavComponent', () => {
 
     // Assert
     expect(TestBed.inject(Router).url).toBe(page)
+  })
+
+  it(`should show the projected content when provided and nav element is expanded (screen larger than mobile).`, () => {
+    // Arrange
+    const testComponent = fixture.debugElement.query(By.directive(TestComponent))
+      .componentInstance as TestComponent
+    testComponent.showExampleSectionContent.set(true)
+    testComponent.expanded.set(true)
+
+    fixture.detectChanges()
+
+    // Act
+    const projectedContentRef = fixture.debugElement.query(
+      By.css(`[data-test="projected-test-content"]`)
+    )
+
+    // Assert
+    expect(projectedContentRef).toBeTruthy()
+  })
+
+  it(`should not show the projected content when provided and nav element is not expanded (screen larger than mobile).`, () => {
+    // Arrange
+    const testComponent = fixture.debugElement.query(By.directive(TestComponent))
+      .componentInstance as TestComponent
+    testComponent.showExampleSectionContent.set(true)
+    testComponent.expanded.set(false)
+    fixture.detectChanges()
+
+    // Act
+    const projectedContentRef = fixture.debugElement.query(
+      By.css(`[data-test="projected-test-content"]`)
+    )
+
+    // Assert
+    expect(projectedContentRef).toBeFalsy()
+  })
+
+  it(`should not show the section element if no content has been passed.`, () => {
+    // Arrange
+    const testComponent = fixture.debugElement.query(By.directive(TestComponent))
+      .componentInstance as TestComponent
+    testComponent.showExampleSectionContent.set(false)
+    testComponent.expanded.set(true)
+    fixture.detectChanges()
+
+    // Act
+    const projectedContentRef = fixture.debugElement.query(
+      By.css(`[data-test="projected-test-content"]`)
+    )
+
+    // Assert
+    expect(projectedContentRef).toBeFalsy()
   })
 })
--- frontend/src/shared/ui/nav/nav.component.ts
+++ frontend/src/shared/ui/nav/nav.component.ts
@@ -1,45 +1,43 @@
-import { NgComponentOutlet } from '@angular/common'
-import { ChangeDetectionStrategy, Component, input } from '@angular/core'
+import { NgClass, NgComponentOutlet, NgTemplateOutlet } from '@angular/common'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  contentChild,
+  input,
+  model,
+  TemplateRef,
+} from '@angular/core'
 import { RouterLink, RouterLinkActive } from '@angular/router'
 import { RoutingLink } from '../../utils/models/routingLink.interface'
+import { ChevronLeftIconComponent } from '../icons/chevron-left/chevron-left-icon.component'
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterLinkActive, NgComponentOutlet],
-  template: `
-    <nav
-      class="view-transition | z-[2] bg-ui-background-light p-1.5 text-text-light sm:rounded-bl-lg dark:bg-ui-background dark:text-text"
-    >
-      <ul data-test="navigation-list" class="flex items-center justify-evenly gap-4 *:text-sm">
-        @for (link of routingLinks(); track link.linkText) {
-          <li
-            data-test="navigation-list-item"
-            class="relative grid aspect-square w-1/5 max-w-20 place-content-center sm:w-full"
-          >
-            <a
-              class="grid cursor-pointer grid-rows-[1fr_auto] justify-items-center p-2 *:z-[1] before:absolute before:inset-0 before:rounded-lg before:opacity-10 hover:before:bg-current focus-visible:before:bg-current motion-safe:before:transition-colors"
-              [routerLink]="link.routerLink"
-              routerLinkActive="before:bg-current before:opacity-20 hover:before:opacity-10"
-              ariaCurrentWhenActive="page"
-            >
-              <div class="row-span-1">
-                <ng-container *ngComponentOutlet="link.component"></ng-container>
-              </div>
-              <span class="row-span-2 text-sm">{{ link.linkText }}</span>
-            </a>
-          </li>
-        }
-      </ul>
-    </nav>
-  `,
-  styles: `
-    .view-transition {
-      view-transition-name: nav;
-    }
-  `,
+  imports: [
+    NgClass,
+    RouterLink,
+    RouterLinkActive,
+    RouterLinkActive,
+    NgComponentOutlet,
+    ChevronLeftIconComponent,
+    NgTemplateOutlet,
+  ],
+  templateUrl: 'nav.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavComponent {
   readonly routingLinks = input.required<RoutingLink[]>()
+  readonly expanded = model.required<boolean>()
+  readonly content = contentChild('navSectionContent', { read: TemplateRef })
+
+  protected readonly iconComponentSizeConfig = { size: 'size-7 sm:size-8' }
+
+  protected readonlyExpanded = this.expanded.asReadonly()
+
+  protected toggleExpanded(force?: boolean | undefined) {
+    const newState = typeof force === 'boolean' ? force : !this.expanded()
+
+    this.expanded.set(newState)
+  }
 }
