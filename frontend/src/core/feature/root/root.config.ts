6d225012a6193d78a2e7104c449f49edbe71c2a7
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2025-01-31 20:45:03 +0100

    feat(core): Add HttpClient provider and APP_CONFIG DI token to the app.

--- frontend/src/core/feature/root/root.config.ts
+++ frontend/src/core/feature/root/root.config.ts
@@ -8,15 +8,19 @@ import {
 import { appRoutes } from './root.routes'
 import { provideClientHydration, withEventReplay, withI18nSupport } from '@angular/platform-browser'
 import { CustomTitleStrategy } from '@lumin/shared/custom-title-strategy/custom-title-strategy.service'
+import { APP_CONFIG, APP_CONFIG_VALUE } from '@lumin/shared/app-config/app-config.token'
+import { provideHttpClient, withFetch } from '@angular/common/http'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay(), withI18nSupport()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes, withViewTransitions(), withComponentInputBinding()),
+    provideHttpClient(withFetch()),
     {
       provide: TitleStrategy,
       useClass: CustomTitleStrategy,
     },
+    { provide: APP_CONFIG, useValue: APP_CONFIG_VALUE },
   ],
 }
