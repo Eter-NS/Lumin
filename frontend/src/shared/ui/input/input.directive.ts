17f5e837f7ba14badbdb34ad888f82935049c470
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2025-02-15 21:09:22 +0100

    feat(frontend/shared): Add custom styles and opacity transition for placeholders in Input directive
    - Added custom styles to Input directive for placeholders,
    - Implemented an opacity transition effect when the user clears an input/textarea,
    - Imported the transition styles to `styles.scss` for global scope.

--- frontend/src/shared/ui/input/input.directive.ts
+++ frontend/src/shared/ui/input/input.directive.ts
@@ -5,7 +5,7 @@ import { Directive } from '@angular/core'
   standalone: true,
   host: {
     class:
-      'flex-1 shrink-0 rounded-lg bg-ui-background-light p-1 text-text-light outline-none transition-colors dark:bg-ui-background dark:text-text',
+      'flex-1 shrink-0 rounded-lg bg-ui-background-light p-1 text-text-light outline-none transition-colors dark:bg-ui-background dark:text-text placeholder:dark:text-text/80 placeholder:text-text-light/80',
   },
 })
 export class InputDirective {}
